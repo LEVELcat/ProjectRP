11cba82a3bf0621017923579dbe1a0d4df170ac9
LEVELcat <stalkerclearsky2000@gmail.com>
2022-09-14 15:27:09 +0300

    test

---
+++ beckend/freemode/freemode/Events.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace freemode
+{
+    class Events : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStarted()
+        {
+            mysql.InitConnection();
+        }
+
+        [ServerEvent(Event.PlayerConnected)]
+        private void OnPlayerConnected(Player player)
+        {
+            player.SendChatMessage("Добро пожаловать на сервер ~r~ШИЗОФРЕНИЯ");
+        }
+
+        [ServerEvent(Event.PlayerSpawn)]
+        private void OnPlayerSpawn(Player player)
+        {
+            player.Health = 100;
+            player.Armor = 0;
+        }
+    }
+}
